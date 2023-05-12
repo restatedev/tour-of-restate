8c5d79401ed0b6aad66d6b9411a3bf1fcad446c9
Giselle van Dongen <giselle.vd@gmail.com>
2023-05-12 15:17:21 +0200

    init

---
+++ src/ticket_service.ts
@@ -0,0 +1,29 @@
+import * as restate from "@restatedev/restate-sdk";
+import {
+    protoMetadata,
+    Ticket,
+    TicketService
+} from "./generated/proto/example";
+import {BoolValue} from "./generated/proto/google/protobuf/wrappers";
+import {Empty} from "./generated/proto/google/protobuf/empty";
+
+class TicketSvc implements TicketService {
+
+    markAsSold(request: Ticket): Promise<Empty> {
+        return Promise.resolve(Empty.create({}));
+    }
+
+    reserve(request: Ticket): Promise<BoolValue> {
+        return Promise.resolve(BoolValue.create({}));
+    }
+
+    unreserve(request: Ticket): Promise<Empty> {
+        return Promise.resolve(BoolValue.create({}));
+    }
+}
+
+restate.createServer().bindService({
+    service: "TicketService",
+    instance: new TicketSvc(),
+    descriptor: protoMetadata,
+});
---
+++ src/user_session_service.ts
@@ -0,0 +1,29 @@
+import * as restate from "@restatedev/restate-sdk";
+import {
+  CheckoutRequest, ExpireTicketRequest,
+  protoMetadata,
+  ReserveTicket,
+  UserSessionService,
+} from "./generated/proto/example";
+import { BoolValue } from "./generated/proto/google/protobuf/wrappers";
+import {Empty} from "./generated/proto/google/protobuf/empty";
+
+class UserSessionSvc implements UserSessionService {
+  addTicket(request: ReserveTicket): Promise<BoolValue> {
+    return Promise.resolve(BoolValue.create({ value: true }));
+  }
+
+  checkout(request: CheckoutRequest): Promise<BoolValue> {
+    return Promise.resolve(BoolValue.create({ value: true }));
+  }
+
+  expireTicket(request: ExpireTicketRequest): Promise<Empty> {
+    return Promise.resolve(BoolValue.create({ value: true }));
+  }
+}
+
+restate.createServer().bindService({
+  service: "UserSessionService",
+  instance: new UserSessionSvc(),
+  descriptor: protoMetadata,
+});
