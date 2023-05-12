69afbf31ff224650ba584ab7480ee3b5cb9cfaf4
Giselle van Dongen <giselle.vd@gmail.com>
2023-05-12 15:18:16 +0200

    format

--- src/ticket_service.ts
+++ src/ticket_service.ts
@@ -1,29 +1,28 @@
 import * as restate from "@restatedev/restate-sdk";
 import {
-    protoMetadata,
-    Ticket,
-    TicketService
+  protoMetadata,
+  Ticket,
+  TicketService,
 } from "./generated/proto/example";
-import {BoolValue} from "./generated/proto/google/protobuf/wrappers";
-import {Empty} from "./generated/proto/google/protobuf/empty";
+import { BoolValue } from "./generated/proto/google/protobuf/wrappers";
+import { Empty } from "./generated/proto/google/protobuf/empty";
 
 class TicketSvc implements TicketService {
+  markAsSold(request: Ticket): Promise<Empty> {
+    return Promise.resolve(Empty.create({}));
+  }
 
-    markAsSold(request: Ticket): Promise<Empty> {
-        return Promise.resolve(Empty.create({}));
-    }
+  reserve(request: Ticket): Promise<BoolValue> {
+    return Promise.resolve(BoolValue.create({}));
+  }
 
-    reserve(request: Ticket): Promise<BoolValue> {
-        return Promise.resolve(BoolValue.create({}));
-    }
-
-    unreserve(request: Ticket): Promise<Empty> {
-        return Promise.resolve(BoolValue.create({}));
-    }
+  unreserve(request: Ticket): Promise<Empty> {
+    return Promise.resolve(BoolValue.create({}));
+  }
 }
 
 restate.createServer().bindService({
-    service: "TicketService",
-    instance: new TicketSvc(),
-    descriptor: protoMetadata,
+  service: "TicketService",
+  instance: new TicketSvc(),
+  descriptor: protoMetadata,
 });
--- src/user_session_service.ts
+++ src/user_session_service.ts
@@ -1,12 +1,13 @@
 import * as restate from "@restatedev/restate-sdk";
 import {
-  CheckoutRequest, ExpireTicketRequest,
+  CheckoutRequest,
+  ExpireTicketRequest,
   protoMetadata,
   ReserveTicket,
   UserSessionService,
 } from "./generated/proto/example";
 import { BoolValue } from "./generated/proto/google/protobuf/wrappers";
-import {Empty} from "./generated/proto/google/protobuf/empty";
+import { Empty } from "./generated/proto/google/protobuf/empty";
 
 class UserSessionSvc implements UserSessionService {
   addTicket(request: ReserveTicket): Promise<BoolValue> {
