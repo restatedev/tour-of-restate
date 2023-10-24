2a60f4c23db1c37201735dba989a7a51a946cb74
Giselle van Dongen <giselle.vd@gmail.com>
2023-10-24 07:08:57 -0700

    Change port to 9080 and small improvements (#9)

--- src/auxiliary/email_client.ts
+++ src/auxiliary/email_client.ts
@@ -13,13 +13,13 @@ export class EmailClient {
   public static get() {
     return new EmailClient();
   }
-  public notifyUserOfPaymentSuccess(userId: string): boolean {
+  async notifyUserOfPaymentSuccess(userId: string): Promise<boolean> {
     console.log(`Notifying user ${userId} of payment success`);
     // send the email
     return true;
   }
 
-  public notifyUserOfPaymentFailure(userId: string): boolean {
+  async notifyUserOfPaymentFailure(userId: string): Promise<boolean> {
     console.log(`Notifying user ${userId} of payment failure`);
     // send the email
     return true;
